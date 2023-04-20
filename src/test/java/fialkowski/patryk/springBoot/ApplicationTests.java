cac7fccc808c855a334df5ee6f1600483789feea
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-04-20 21:04:17 +0200

    Initial commit

---
+++ src/test/java/fialkowski/patryk/springBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fialkowski.patryk.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
