a361f9f2a21dc8c6a3c1fdd874b70724d351d597
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-04-20 22:29:48 +0200

    spring boot web

--- src/main/java/fialkowski/patryk/springBoot/Application.java
+++ src/main/java/fialkowski/patryk/springBoot/Application.java
@@ -1,20 +1,13 @@
 package fialkowski.patryk.springBoot;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("work");
-	}
 }
